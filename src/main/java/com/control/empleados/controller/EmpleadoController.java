9626a5fef286656416efeb7ddef1860c19baf59d
Andres Fernandez <andres94.af@gmail.con>
2023-04-04 18:16:30 +0200

    agregando metodos crud al controller, agregar vista para crear o editar empleado y agregar errores al layout

--- src/main/java/com/control/empleados/controller/EmpleadoController.java
+++ src/main/java/com/control/empleados/controller/EmpleadoController.java
@@ -1,13 +1,22 @@
 package com.control.empleados.controller;
 
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.control.empleados.model.Empleado;
 import com.control.empleados.service.IEmpleadoService;
@@ -20,6 +29,18 @@ public class EmpleadoController {
 	private IEmpleadoService empleadoService;
 
 	
+	@GetMapping("/ver/{id}")
+	public String verDetalleDeEmpleado(@PathVariable Long id, Map<String, Object> modelo, RedirectAttributes flash) {
+		Empleado empleado = empleadoService.findOne(id);
+		if (empleado == null) {
+			flash.addFlashAttribute("error", "El id del empleado no existe en la BBDD");
+			return "redirect:/listar";
+		}
+		modelo.put("empleado", empleado);
+		modelo.put("titulo", "Detalles del empleado"+empleado.getNombre());
+		return "detalles_empleado";
+	}
+	
 	@GetMapping({"/", "/listar", ""})
 	public String listarEmpleados(@RequestParam(name="page", defaultValue = "0") int page, Model modelo) {
 		Pageable pageRequest = PageRequest.of(page, 5);
@@ -30,5 +51,55 @@ public class EmpleadoController {
 		modelo.addAttribute("page", pageRender);
 		return "listar";
 	}
+	
+	@GetMapping("/form")
+	public String mostrarFormularioDeRegistro(Map<String, Object> modelo) {
+		Empleado empleado = new Empleado();
+		modelo.put("empleado", empleado);
+		modelo.put("titulo", "Registro de empleado");
+		return "formulario";
+	}
+	
+	@PostMapping("/form")
+	public String guardarEmpleado(@Valid Empleado empleado, BindingResult result, Model modelo, RedirectAttributes flash, SessionStatus status) {
+		if (result.hasErrors()) {
+			modelo.addAttribute("titulo", "Registro de empleado");
+			return "formulario";
+		}
+		String mensaje = (empleado.getId() != null) ? "El empleado ha sido editado con exito" : "Empleado registrado con exito";
+		
+		empleadoService.save(empleado);
+		status.setComplete();
+		flash.addFlashAttribute("success", mensaje);
+		return "redirect:/listar";
+	}
+	
+	@GetMapping("/form/{id}")
+	public String editarEmpleado(@PathVariable Long id, Map<String, Object> modelo, RedirectAttributes flash) {
+		Empleado empleado = null;
+		if (id > 0) {
+			empleado = empleadoService.findOne(id);
+			if (empleado == null) {
+				flash.addFlashAttribute("error", "El id del empleado no existe en la BBDD");
+				return "redirect:/listar";
+			}
+		}else {
+			flash.addFlashAttribute("error", "El ID del empleado no puede ser 0");
+			return "redirect:/listar";
+		}
+		modelo.put("empleado", empleado);
+		modelo.put("titulo", "Edición del empleado");
+		return "formulario";
+	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eliminarEmpleado(@PathVariable Long id, RedirectAttributes flash) {
+		if (id > 0) {
+			empleadoService.delete(id);
+			flash.addFlashAttribute("success", "Empleado eliminado con exito");
+		}
+		return "redirect:/listar";
+	}
+	
 	
 }
