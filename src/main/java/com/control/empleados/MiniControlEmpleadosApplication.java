ebb027055f79daac324a30f07a5b378ca3cce1d2
Andres Fernandez <andres94.af@gmail.con>
2023-04-04 11:36:05 +0200

    creando proyecto, modelo base de empleado

---
+++ src/main/java/com/control/empleados/MiniControlEmpleadosApplication.java
@@ -0,0 +1,13 @@
+package com.control.empleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniControlEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniControlEmpleadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/control/empleados/MiniControlEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.control.empleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniControlEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
